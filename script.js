e0ba0c87119be1786f5de6b14d2009114df5beca
Bhairav Sharma <bhairavsharma129@gmail.com>
2024-10-19 09:04:39 +0530

    restoring newer version

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-var head1=document.getElementsByTagName("h1")
-head1[0].addEventListener("click",function(){alert("hello there!");})
+function typeWriter(text, element) {
+    const speed = 85; // Adjust typing speed in milliseconds
+    const caret = document.createElement('span');
+    caret.classList.add('caret');
+  
+    element.appendChild(caret);
+  
+    let i = 0;
+    let txt = text;
+  
+    function type() {
+      if (i < txt.length) {
+        const char = txt.charAt(i);
+        const charSpan = document.createElement('span');
+        charSpan.textContent = char;
+        element.insertBefore(charSpan, caret);
+        i++;
+        setTimeout(type, speed);
+      } else {
+        caret.style.display = 'inline'; // Show the caret after typing
+      }
+    }
+  
+    type();
+  }
+  
